1a74ae28de1629c9660eb40ae88b7b8512d7d690
Tortapounder3000 <rowanmemba7@gmail.com>
2026-03-14 12:38:34 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-  user: 'root',
-  password: 'Root@NECTA2025!',
-  database: 'nectaassist_db',
-  socketPath: '/tmp/mysql.sock' 
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 module.exports = db;
